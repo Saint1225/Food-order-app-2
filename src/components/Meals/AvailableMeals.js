6a5326f8ef299433d95af8f9896a949a2a540ef1
Saint1225 <vincent-valentine1225@hotmail.com>
2024-01-19 22:02:23 +0800

    Code formatted

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,11 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-
   const [loadedMeals, setLoadedMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
@@ -16,28 +15,28 @@ const AvailableMeals = () => {
     const fetchData = async () => {
       clearTimeout(timer);
       try {
-        const response = await fetch("https://react-http-2bc42-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json", {
-          method: "GET"
-        });
+        const response = await fetch(
+          "https://react-http-2bc42-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json",
+          {
+            method: "GET",
+          }
+        );
         if (!response.ok) {
-          throw new Error ("Fetch issue!");
+          throw new Error("Fetch issue!");
         }
         const meals = await response.json();
-        const mealsArray = Object.keys(meals).map(key => (
-            {
-              id: key,
-              name: meals[key].name,
-              description: meals[key].description,
-              price: meals[key].price,
-            })
-          );
+        const mealsArray = Object.keys(meals).map((key) => ({
+          id: key,
+          name: meals[key].name,
+          description: meals[key].description,
+          price: meals[key].price,
+        }));
         setLoadedMeals(mealsArray);
         timer = setTimeout(() => {
           setIsLoading(false);
-        }, 2000)
-      }
-      catch (error) {
-        console.log(error)
+        }, 2000);
+      } catch (error) {
+        console.log(error);
         setError(error.message);
         setIsLoading(false);
       }
